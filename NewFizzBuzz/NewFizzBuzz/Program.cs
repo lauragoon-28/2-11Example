fc9e4366589d31d0a8d8078c3a2be67bc6cc7fe8
Laura Goon <Laura.goon@ou.edu>
2021-02-11 17:39:43 -0600

    Finished

---
+++ NewFizzBuzz/NewFizzBuzz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace NewFizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int randomNumber = rand.Next(1, 101);//Generate a random number between 1 and 100
+            int divisibleBy3Remainder = randomNumber % 3;
+            bool isDivisibleBy3 = randomNumber % 3 == 0;
+            randomNumber = 20;
+
+            if (randomNumber % 3 == 0
+              && randomNumber % 5 == 0)// === true)
+            {
+                Console.WriteLine("Fizz Buzz");
+            }
+            else if (randomNumber % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else if (randomNumber % 3 ==0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else
+            {
+                Console.WriteLine(randomNumber);
+            }
+            
+        }
+    }
+}
